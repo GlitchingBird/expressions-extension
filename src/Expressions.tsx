261e439d8a53259074f13b8a8497a0b019c7ddd2
GlitchingBird <nikitos2003331@gmail.com>
2025-02-25 13:13:07 +0400

    Expressions.tsx

--- src/Expressions.tsx
+++ src/Expressions.tsx
@@ -38,6 +38,11 @@ enum EmotionEnum {
     sadness = 'sadness',
     surprise = 'surprise',
     neutral = 'neutral',
+    1 = '1',
+    2 = '2',
+    3 = '3',
+    4 = '4',
+    5 = '5',
 }
 
 type Emotion = {
